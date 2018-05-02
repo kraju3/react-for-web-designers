45b7b68d1a5c9fc3de1dbd7b312be0bc7ec47f08
Kiran Raju <kraju3@uic.edu>
2018-05-02 05:34:18 +0000

    started using babel

--- Exercise Files/Ch02/02_04/start/app.js
+++ Exercise Files/Ch02/02_04/start/app.js
@@ -1,27 +1,25 @@
 (function() {
   "use strict";
+  
+  var rce=React
 
   function ProductImage(props) {
-    return React.createElement("img", {
-      src: "../../../assets/red.jpg",
-      alt: "Product Image"
-    });
+    return <img src= "../../../assets/red.jpg" alt= "Product Image"/>;
   }
 
   function ProductCustomizer(props) {
-    return React.createElement(
-      "div",
-      { className: "customizer" },
-      React.createElement(
-        "div",
-        { className: "product-image" },
-        React.createElement(ProductImage)
-      )
+  
+return {
+  <div className="customizer">
+  <div className="product-image">
+    <ProductImage/>
+    </div>
+    </div>
     );
-  }
+}
 
   ReactDOM.render(
-    React.createElement(ProductCustomizer),
+    <ProductCustomizer/>,
     document.getElementById("react-root")
   );
 })();
