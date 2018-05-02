858ddd20be91c2bb389f1ab33469c045b73d5b89
Kiran Raju <kraju3@uic.edu>
2018-05-02 05:52:35 +0000

    create a new component with array data

--- Exercise Files/Ch02/02_05/start/app.js
+++ Exercise Files/Ch02/02_05/start/app.js
@@ -1,5 +1,29 @@
 (function() {
   "use strict";
+  
+  function SizeSelector(){
+    return{
+      <div className="field-group">
+								<label htmlFor="size-options">Size:</label>
+								<select name="sizeOptions" id="size-options">
+									<option>7</option>
+									<option>7.5</option>
+									<option>8</option>
+									<option>8.5</option>
+									<option>9</option>
+									<option>9.5</option>
+									<option>10</option>
+									<option>10.5</option>
+									<option>11</option>
+									<option>11.5</option>
+									<option>12</option>
+									<option>12.5</option>
+								</select>
+							</div>
+						);
+    }
+    
+  }
 
   function ProductImage(props) {
     return <img src="../../../assets/red.jpg" alt="Product Image" />;
@@ -11,6 +35,8 @@
         <div className="product-image">
           <ProductImage />
         </div>
+        <div className="selectors">
+          <SizeSelector/>
       </div>
     );
   }
